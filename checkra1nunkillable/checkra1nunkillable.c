dc9fc22e9f57da6f0a67a7e6684f2516a0568c76
Jus de Patate_ <jusdepatate@protonmail.com>
2020-02-24 15:32:16 +0100

    `checkra1nunkillable` checks window size
    this was made to remove infinite error on `checkra1nunkillable` if your terminal size is less than 80x24

--- checkra1nunkillable/checkra1nunkillable.c
+++ checkra1nunkillable/checkra1nunkillable.c
@@ -5,14 +5,28 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 void SIGhandler(int);
 
 int main(void) {
 	signal(SIGINT, SIGhandler);
 	signal(SIGQUIT, SIGhandler);
-	while (1)
-		system("/usr/bin/checkra1n --tui");
+
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    while (1) {
+        if (( w.ws_col <= 80 || w.ws_row < 24)) {
+            // if terminal size is less than 80x24 (minimum required by checkra1n), use it in CLI mode instead of TUI
+            fprintf(stderr, "\033[0;31mYour terminal is too small to start checkra1n in TUI mode, using CLI mode instead.\033[0m\nYou can resize and use CTRL+C to use TUI mode\n");
+            system("/usr/bin/checkra1n --cli");
+
+        } else {
+            system("/usr/bin/checkra1n --tui");
+        }
+    }
 	return 0;
 }
 
